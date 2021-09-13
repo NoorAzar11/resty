f3e8f274e73c42ddaa1f8b005bcc2bb074e8d5a2
NoorAzar11 <nooraaz945@gamil.com>
2021-09-13 19:21:19 +0300

    class26

---
+++ starter-code/src/app.js
@@ -0,0 +1,90 @@
+import React,{useState} from 'react';
+
+import './app.scss';
+
+// Let's talk about using index.js and some other name in the component folder
+// There's pros and cons for each way of doing this ...
+import Header from './components/header';
+import Footer from './components/footer';
+import Form from './components/form';
+import Results from './components/results';
+
+
+function App(props){
+  const [state,setState]=useState({data:'',requestParams:{}});
+
+  function callApi(requestParams){
+   
+      // mock output
+      const data = {
+        count: 2,
+        results: [
+          {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+          {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+        ],
+      };
+      setState({data, requestParams});
+    }
+    return (
+      <React.Fragment>
+        <Header />
+        <div>Request Method: {state.requestParams.method}</div>
+        <div>URL: {state.requestParams.url}</div>
+        <Form handleApiCall={callApi} />
+        <Results data={state.data} />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+  
+
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: null,
+//       requestParams: {},
+//     };
+//   }
+
+//   callApi = (requestParams) => {
+//     // mock output
+//     const data = {
+//       count: 2,
+//       results: [
+//         {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+//         {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+//       ],
+//     };
+//     this.setState({data, requestParams});
+//   }
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Header />
+//         <div>Request Method: {this.state.requestParams.method}</div>
+//         <div>URL: {this.state.requestParams.url}</div>
+//         <Form handleApiCall={this.callApi} />
+//         <Results data={this.state.data} />
+//         <Footer />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+export default App;
---
+++ starter-code/src/components/footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+  
+    return <footer>&copy; React Resty 2021 application </footer>;
+  
+}
+
+export default Footer;
